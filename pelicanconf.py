a8233d7ad0208605c1a6236cfec3c801ab4e4ec4
Mark Biek <mark.biek@kindred.com>
2015-12-17 09:01:03 -0500

    Updates for ssl support

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 
 AUTHOR = u'Mark Biek'
 SITENAME = u'Antelope Love Fan'
-SITEURL = 'http://mark.biek.org/blog'
+SITEURL = '//mark.biek.org/blog'
 ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}/'
 
 FEED = 'feeds/all.atom.xml'
@@ -18,7 +18,6 @@ DISQUS_SITENAME = 'markbiekorg'
 GOOGLE_ANALYTICS = 'UA-6890403-1'
 
 FILES_TO_COPY = (('extra/.htaccess', '.htaccess'),)
-#THEME = "pelican-themes/mnmlist"
 THEME = "pelican-themes/alftheme"
 
 # Blogroll
@@ -26,10 +25,10 @@ LINKS =  ()
 
 # Social widget
 SOCIAL = (
-            ('<img src="http://static.biek.org/blog/img/twitter-128-black.png" alt="@antelopelovefan" />', 'https://twitter.com/antelopelovefan'),
-            ('<img src="http://static.biek.org/blog/img/stackoverflow-128-black.png" alt="Stackoverflow Careers" />', 'http://careers.stackoverflow.com/markbiek'),
-            ('<img src="http://static.biek.org/blog/img/github-128-black.png" alt="GitHub" />', 'https://github.com/markbiek?tab=repositories'),
-            ('<img src="http://static.biek.org/blog/img/instagram-128-black.png" alt="Instagram" />', 'http://instagram.com/markbiek/'),
+            ('<img src="//static.biek.org/blog/img/twitter-128-black.png" alt="@antelopelovefan" />', 'https://twitter.com/antelopelovefan'),
+            ('<img src="//static.biek.org/blog/img/stackoverflow-128-black.png" alt="Stackoverflow Careers" />', 'http://careers.stackoverflow.com/markbiek'),
+            ('<img src="//static.biek.org/blog/img/github-128-black.png" alt="GitHub" />', 'https://github.com/markbiek?tab=repositories'),
+            ('<img src="//static.biek.org/blog/img/instagram-128-black.png" alt="Instagram" />', 'http://instagram.com/markbiek/'),
             ('<span class="oi oi-lock-locked" title="lock-locked" aria-hidden="true"></span>', 'http://mark.biek.org/pub.asc'),
         )
 
