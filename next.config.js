6a7225c52c718b26e9fcd0267efe50cb028da7fe
Mark Biek <support@janustech.net>
2021-10-28 10:38:42 -0400

    Initial redirect setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+const { getRedirectStatus } = require('next/dist/lib/load-custom-routes');
+
 module.exports = {
 	reactStrictMode: true,
 	images: {
 		domains: ['static.biek.org'],
+	},
+	async redirects() {
+		return [
+			{
+				source: '/links',
+				destination: '/contact',
+				permanent: true,
+			},
+		];
 	},
 };
