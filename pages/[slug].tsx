a4d7ebdeabaf857f14a95c96b24249c2dcb0380e
Mark Biek <mark.biek@automattic.com>
2022-03-15 15:19:54 -0400

    WIP: other pages

---
+++ pages/[slug].tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { getPage } from "../lib/api";
+
+import Layout from "../views/Layout";
+
+export default function ViewPage() {
+	return (
+		<Layout>
+			<article></article>
+		</Layout>
+	);
+}
+
+export async function getStaticProps({ preview = false }) {
+	const page = await getPage("/");
+
+	console.log(page);
+
+	return {
+		props: {
+			page: page?.pageBy,
+		},
+	};
+}
