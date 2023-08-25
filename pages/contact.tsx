21e8f9d800f5adf4dcf0acf45bd5ad9239a62c4a
Mark Biek <mark.biek@automattic.com>
2023-08-25 06:55:53 -0400

    Make Contact page content managed

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,35 +1,31 @@
 import Layout from "../views/Layout";
 
-export default function Contact() {
+import { CmsPageType } from "../types";
+import { getWordPressPostBySlug } from "../lib/api";
+
+interface ContactProps {
+	post: CmsPageType;
+}
+
+export default function Contact({post}: ContactProps) {
+	const { title, content } = post;
+
 	return (
 		<Layout>
 			<article>
-				<h1>Contact Me</h1>
+				<h1>{title}</h1>
 
-				<ul>
-					<li>
-						<a href="mailto:info@biek.org">info@biek.org</a>
-					</li>
-					<li>
-						<a href="https://github.com/markbiek">Github</a>
-					</li>
-					<li>
-						<a href="https://m.antelopelovefan.com/@antelopelovefan">
-							Mastodon
-						</a>
-					</li>
-				</ul>
-			</article>
-			<article>
-				<h2>I don't have accounts on any freelance sites</h2>
-
-				<p>
-					If you happen to see a Mark Biek with my picture on Upwork or any
-					other freelance sites, that's a fake profile. I don't currently do
-					work through any freelance sites, though I am occasionally available
-					for contract work.
-				</p>
+				<div dangerouslySetInnerHTML={{ __html: content }} />
 			</article>
 		</Layout>
 	);
+}
+
+export async function getStaticProps() {
+	const post = await getWordPressPostBySlug("contact");
+
+	return {
+		props: { post },
+		revalidate: 300,
+	};
 }
