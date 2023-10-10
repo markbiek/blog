f2e823b4875084f1cd0774f22bc398b5ea88fa15
Mark Biek <mark.biek@automattic.com>
2023-10-10 12:06:47 -0400

    Script to generate a new post

---
+++ bin/create-post.js
@@ -0,0 +1,45 @@
+// generate-markdown.js
+
+const readline = require("readline");
+const fs = require("fs");
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+const promptQuestion = (question) => {
+	return new Promise((resolve) => {
+		rl.question(question, (answer) => {
+			resolve(answer);
+		});
+	});
+};
+
+const generateSlug = (title) => {
+	return title.toLowerCase().replace(/\s+/g, "-");
+};
+
+(async () => {
+	const title = await promptQuestion("Title: ");
+	const category = await promptQuestion("Category: ");
+	const tags = await promptQuestion("Tags (comma separated): ");
+
+	const date = new Date();
+	const formattedDate = `${date.getFullYear()}-${String(
+		date.getMonth() + 1
+	).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}`;
+	const slug = generateSlug(title);
+	const filename = `_posts/${formattedDate}-${slug}.md`;
+
+	const content = `Title: ${title}
+Date: ${date.toISOString()}
+Author: mark
+Category: ${category}
+Tags: ${tags}
+Slug: ${slug}`;
+
+	fs.writeFileSync(filename, content);
+	console.log(`Markdown file created: ${filename}`);
+	rl.close();
+})();
