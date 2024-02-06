bd12985973dde1d8e2c8bb0b30e5fb5dd9b89fe9
Mark Biek <mark.biek@automattic.com>
2024-02-06 16:35:48 -0500

    Update to allow html in markdown

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,8 +1,17 @@
-import remark from 'remark';
-import html from 'remark-html';
+import {unified} from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeStringify from 'rehype-stringify'
 
 export default async function markdownToHtml(markdown: string) {
-	const result = await remark().use(html).process(markdown);
+	const result = await unified()
+		.use(remarkParse)
+		.use(remarkRehype, {allowDangerousHtml: true}) // Pass raw HTML strings through.
+		.use(rehypeStringify, {allowDangerousHtml: true}) // Serialize the raw HTML strings
+		.process(markdown)
+		.catch((error) => {
+		throw error
+		})
 
 	return result.toString();
 }
